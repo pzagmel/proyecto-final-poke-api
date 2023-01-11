6527363a6e0e0f9174186fb4c63ab58c8a606c46
pzagmel <pedro.zagalm@gmail.com>
2023-01-11 19:43:21 +0000

    comit cartas y reto del dia

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -3,7 +3,7 @@ import React from "react";
 export const Card = ({img, description, img2}) => {
 
     return (
-        <div className="card" style={{"width": "18rem"}}>
+        <div className="card - bg-primary" style={{"width": "18rem"}}>
         <img src={img} className="card-img-top" alt="..."/>
         <div className="card-body">
           <p className="card-text">{description}</p>
@@ -13,5 +13,21 @@ export const Card = ({img, description, img2}) => {
     );
   };
   
+  export const Longcard = ({img,title,description}) => {
+    return (
+      <div className="card mb-3" style={{"max-width":"540px;"}}>
+  <div className="row g-0">
+    <div className="col-md-4">
+      <img src={img} className="img-fluid rounded-start" alt="..."/>
+    </div>
+    <div className="col-md-8">
+      <div className="card-body">
+        <h5 className="card-title">{title}</h5>
+        <p className="card-text">{description}</p>
+      </div>
+    </div>
+  </div>
+</div>
 
-  
+      );
+    };
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -6,19 +6,57 @@ import unete from "../../img/UNETETITULO.png";
 import pokebola from "../../img/pngegg 1.png";
 import "../../styles/home.css";
 import { Card } from "../component/card.js";
+import { Longcard } from "../component/card.js";
 
-export const Home = () => (
-  <div className="container d-flex justify-content-center">
-    <div className="row">
-      <div className="col 4 text-center" />
-      <Card img={tituloinfo} description={"Informacion de qué trata la app"} />{" "}
-      <div className="col 4 text-center" />
-      <Card
-        img={profesTitulo}
-        description={"Conoce a nuestros profesionales"}
-      />
-      <div className="col 4 text-center" />
-      <Card img={unete} img2={pokebola} />
-    </div>
-  </div>
-);
+export const Home = () => {
+  return (
+    <>
+      <div className="container d-flex justify-content-center">
+        <div className="row">
+          <div className="col" />
+          <Card
+            img={tituloinfo}
+            description={"Informacion de qué trata la app"}
+          />
+          <div className="col" />
+          <Card
+            img={profesTitulo}
+            description={"Conoce a nuestros profesionales"}
+          />
+          <div className="col" />
+          <Card img={unete} img2={pokebola} />
+        </div>
+      </div>
+      <div className="container d-flex justify-content-center">
+        <div className="row">
+          <div className="col">
+            <Longcard
+              title={"Reto de la Semana"}
+              img={
+                "https://lmimirror3pvr.azureedge.net/static/media/18461/22b355e1-db06-4491-b67c-f00048f78b99/training-load-960x540.jpg"
+              }
+              description=
+              {"-100 Burpies y 100 sentadillas"}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="container d-flex justify-content-center">
+        <div className="row">
+          <div className="col" />
+          <Card
+            img={tituloinfo}
+            description={"Profesor 1"}
+          />
+          <div className="col" />
+          <Card
+            img={profesTitulo}
+            description={"Profesor 2"}
+          />
+          <div className="col" />
+          <Card img={unete} description={"Profesor 3"} />
+        </div>
+      </div>
+    </>
+  );
+};
