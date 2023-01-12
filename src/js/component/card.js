9cb17c52d825d1eba87bdf076c874a9983560804
pzagmel <pedro.zagalm@gmail.com>
2023-01-12 16:57:57 +0000

    last commit

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -1,43 +1,56 @@
 import React from "react";
 
 /* componente card para las primer 3 cartas de home */
-export const Card = ({img, description, img2}) => {
-    return (
-        <div className="card - bg-primary" style={{"width": "18rem"}}>
-        <img src={img} className="card-img-top mx-1" />
+export const Card = ({ img, description, img2 }) => {
+  return (
+    <div className="card bg-primary h-100">
+      <img src={img} className="card-img" />
+      <div className="card-body">
+        {description && <p className="card-text text-center">{description}</p>}
+      </div>
+      <img src={img2} className="card-img" />
+    </div>
+  );
+};
+
+export const Longcard = ({
+  img,
+  title,
+  description,
+  description2,
+  description3,
+}) => {
+  return (
+    <div className="card - bg-primary mb-3">
+      <img
+        src={img}
+        className="card-img"
+        // style={{ width: "70rem", height: "18rem" }}
+      />
+
         <div className="card-body">
-          <p className="card-text text-center">{description}</p>
-          <img src={img2} className="card-img-bot" style={{"width": "14rem"}}/>
+          <h5 id="titlereto" className="card-title text-center">
+            {title}
+          </h5>
+          <p id="reto" className="card-text -lg my-4">
+            {description}
+          </p>
+          <p id="reto" className="card-text -lg">
+            {description2}
+          </p>
+          <p id="reto" className="card-text -lg">
+            {description3}
+          </p>
         </div>
       </div>
-    );
-  };
-  
-  export const Longcard = ({img,title,description,description2, description3}) => {
-    return (
-      <div className="card - bg-primary mb-3" style={{"width":"70rem"}}>
-  <div className="row g-0">
-    <div className="col-md-4">
-      <img src={img} className="img-fluid rounded-start" style={{"width":"70rem", "height":"18rem"}}/>
-    </div>
-    <div className="col-md-8">
-      <div className="card-body">
-        <h5 id="titlereto" className="card-title text-center">{title}</h5>
-        <p id="reto" className="card-text -lg my-4">{description}</p>
-        <p id="reto" className="card-text -lg">{description2}</p>
-        <p id="reto" className="card-text -lg">{description3}</p>
-      </div>
-    </div>
-  </div>
-</div>
-);
+  );
 };
 
 /* componente card para los 3 profesionales solo destructuring de imagen */
-export const Procard = ({img}) => {
+export const Procard = ({ img }) => {
   return (
-      <div className="card" style={{"width": "18rem"}}>
-      <img src={img} className="img-fluid"></img>
+    <div className="card">
+      <img src={img} className="card-img"></img>
     </div>
   );
 };
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -4,34 +4,36 @@ import tituloinfo from "../../img/TITULOINFO.png";
 import profesTitulo from "../../img/PROFESTITULO.png";
 import unete from "../../img/UNETETITULO.png";
 import pokebola from "../../img/pngegg 1.png";
-
+import personaltrainer1 from "../../img/personaltrainer1.gif";
+import kine from "../../img/kine.gif";
+import personaltrainer2 from "../../img/personaltrainer2.gif";
 
 import "../../styles/home.css";
 import { Card } from "../component/card.js";
 import { Longcard } from "../component/card.js";
+import { Procard } from "../component/card.js";
 
 export const Home = () => {
   return (
-    <>
-      <div className="container d-flex justify-content-center">
-        <div className="row row-cols-4 g-3">
-          <div className="col-sm" />
-          <Card
-            img={tituloinfo}
-            description={"Informacion de qué trata la app"}
-          />
-          <div className="col-sm" />
-          <Card
-            img={profesTitulo}
-            description={"Conoce a nuestros profesionales"}
-          />
-          <div className="col-sm" />
-          <Card img={unete} img2={pokebola} />
-         
-        
+
+      <div className="container justify-content-center">
+        <div className="row g-3 mb-2">
+          <div className="col-sm-4 ">
+            <Card
+              img={tituloinfo}
+              description={"Informacion de qué trata la app"}
+            />
+          </div>
+          <div className="col-sm-4 ">
+            <Card
+              img={profesTitulo}
+              description={"Conoce a nuestros profesionales"}
+            />
+          </div>
+          <div className="col-sm-4 ">
+            <Card img={unete} img2={pokebola} />
+          </div>
         </div>
-      </div>
-      <div className="container d-flex justify-content-center">
         <div className="row">
           <div className="col">
             <Longcard
@@ -39,28 +41,24 @@ export const Home = () => {
               img={
                 "https://lmimirror3pvr.azureedge.net/static/media/18461/22b355e1-db06-4491-b67c-f00048f78b99/training-load-960x540.jpg"
               }
-              description= {"-100 Burpies"}  
+              description={"-100 Burpies"}
               description2={"-50 Sentadillas Bulgaras"}
-              description3={"-Plancha de 1 minuto"}          
+              description3={"-Plancha de 1 minuto"}
             />
           </div>
         </div>
-      </div>
-      <div className="container d-flex justify-content-center">
         <div className="row">
-          <div className="col" />
-          <Card
-            
-            description={"Profesional 1"}
-          />
-          <div className="col" />
-          <Card
-            description={"Profesional 2"}
-          />
-          <div className="col" />
-          <Card img={unete} description={"Profesional 3"} />
+          <div className="col-sm-4">
+            <Procard img={personaltrainer1} />
+          </div>
+          <div className="col-sm-4">
+            <Procard img={kine} />
+          </div>
+          <div className="col-sm-4">
+            <Procard img={personaltrainer2} />
+          </div>
         </div>
       </div>
-    </>
+
   );
 };
