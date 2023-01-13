1a395ceeaa15a7924bf4ff592b3e82b2cd60d618
LukasOy <lukas.oylu@gmail.com>
2023-01-13 22:20:30 +0000

    13.01 Register

--- src/js/views/register.js
+++ src/js/views/register.js
@@ -1,6 +1,8 @@
 import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/register.css";
+import registro from "../../img/registro.png";
+import LOGOPOKEGYM from "../../img/LOGOPOKEGYM.png";
 
 import { InfoGym } from "../component/CardsINFOGYM";
 import { Footer } from "../component/footer";
@@ -9,9 +11,26 @@ import { Footergp } from "../component/footer2";
 
 
 export const Register = () => (
-	<div className="text-center mt-5">
-		<h1>Pokeregistrate!</h1>
-		
-		
+	
+	<div className="container">
+		<div className="row">
+			<div className="col-1">
+				
+		<div> <img src={LOGOPOKEGYM} style={{width:250}} /></div>
+	<div className="login-card">
+		<h2><img src={registro} style={{width:300}}/></h2>
+		<h3>Introduce Información</h3>
+		<form className="login-form">
+			<input type="text" placeholder="Nombre de Usuario"/>
+			<input type="mail" placeholder="Correo"/>
+			<input type="password" placeholder="Contraseña"/>
+			<input type="password" placeholder="Repetir Contraseña"/>
+			<input type="telefono" placeholder="+569"/>
+			<input type="Rut" placeholder="Rut"/>
+			<button>CREAR</button>
+		</form>
+		</div>
+		</div>
+	</div>
 	</div>
 );
