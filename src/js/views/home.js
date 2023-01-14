1e1df13f452cd1b05fd5ab0e2f4489e9a6670d30
jeremyAparicio <jeremy.aparicio.g@gmail.com>
2023-01-14 01:48:32 +0000

    Se creo el componente de carousel

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -2,10 +2,12 @@ import React from "react";
 import "../../styles/home.css";
 import { CartHome } from "../component/cardhome";
 import { Calendario } from "../component/calendario";
+import { Comments }  from "../component/carousel";
 
 export const Home = () => (
 	<div>
 		<CartHome/>
 		<Calendario/>
+		<Comments/>
 	</div>
 );
