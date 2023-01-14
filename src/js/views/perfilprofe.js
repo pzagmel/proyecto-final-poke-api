2d2488df0db00aaf71e96786c058ccdc60a22407
LukasOy <lukas.oylu@gmail.com>
2023-01-14 01:43:47 +0000

    perfilprofesional

--- src/js/views/perfilprofe.js
+++ src/js/views/perfilprofe.js
@@ -4,9 +4,15 @@ import "../../styles/perfilprofe.css";
 import { InfoGym } from "../component/CardsINFOGYM";
 import { Footer } from "../component/footer";
 import { Footergp } from "../component/footer2";
+import { FichaPersonal } from "../component/FijaPersonal";
+import { ListaClientes } from "../component/PerfilProfesionalListaClientes";
 
 export const Perfilprofe = () => (
         <div> <h1> PerfilProfe</h1>
+
+        <FichaPersonal/>
+        <ListaClientes/>
+        
             </div>
 
 
