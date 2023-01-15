4e7cac4729c86abbbc6567e9920be51ce20cf083
pzagmel <pedro.zagalm@gmail.com>
2023-01-15 19:56:39 +0000

    ficha perfil profe

--- src/js/component/fichaevaluacionpro.js
+++ src/js/component/fichaevaluacionpro.js
@@ -1,33 +1,33 @@
 import React from "react";
-
+import Machop from "../../img/Machop.png";
 export const Fichaevaluacionpro = ()=> {
     return(
-<div className="card" style={{"width": "50rem;"}}>
+<div className="card">
   <div className="card-body">
-    <h5 className="card-title">Ficha de evaluacion profesional</h5>
+    <h5 className="card-titleficha text-center">Ficha de evaluacion profesional</h5>
     <h6 className="card-subtitle mb-2 text-muted">Informacion general</h6>
     
-        <div className="table">
-            <tr>
-                <td>Peso</td>
-                <td>70kg</td>
-            </tr>
-            <tr>
-                <td>Talla</td>
-                <td>170cm</td>
-            </tr>
-            <tr>
-                <td>% de Grasa</td>
-                <td>40%</td>
-            </tr>
-            <tr>
-                <td>% de Masa Muscular</td>
-                <td>28%</td>
-            </tr>
-        </div>
-    <p className="card-text">
-        <div>Objetivo: Ganar masa muscular, disminuir % de grasa, mejorar capacidad aerobica</div>
-    <div> Retroalomentacion del profesional:</div>
+    <table className="table">
+  <thead>
+    <tr>
+      <th scope="col">Peso</th>
+      <th scope="col">Estatura</th>
+      <th scope="col">% de Grasa</th>
+      <th scope="col">% de Músculo</th>
+    </tr>
+  </thead>
+  <tbody className="table-group-divider">
+    <tr>
+      <td scope="row">70kg</td>
+      <td>170cm</td>
+      <td>40%</td>
+      <td>25%</td>
+    </tr>
+    </tbody>
+    </table>
+    <p className="cardficha-text">
+        <p>Objetivo: Ganar masa muscular, disminuir % de grasa, mejorar capacidad aerobica</p>
+    <p> Retroalomentacion del profesional: </p>
     </p>  
 
   </div>
@@ -36,15 +36,15 @@ export const Fichaevaluacionpro = ()=> {
 };
 export const Fichaejercicios = ()=> {
     return(
-<div className="card mb-3" style={{"max-width": "540px;"}}>
+<div className="card mb-3" >
   <div className="row g-0">
     <div className="col-md-4">
-      <img src="..." className="img-fluid rounded-start" alt="..."/>
+      <img src={Machop} className="img-fluid rounded-start" alt="..."/>
     </div>
     <div className="col-md-8">
       <div className="card-body">
         <h5 className="card-title">TUS EJERCICIOS</h5>
-        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p className="card-text">Aqui van los ejercicios</p>
       </div>
     </div>
   </div>
