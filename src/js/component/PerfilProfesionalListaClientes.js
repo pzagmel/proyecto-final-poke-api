7844014cc8f2adc05a96babc8e265729fa8b24b9
LukasOy <lukas.oylu@gmail.com>
2023-01-17 20:30:35 +0000

    button arreglado

--- src/js/component/PerfilProfesionalListaClientes.js
+++ src/js/component/PerfilProfesionalListaClientes.js
@@ -2,17 +2,31 @@ import React from "react";
 import pokebola from "../../img/POKEBOLA.png"
 import "../../styles/PPClientes.css";
 
+window.onload = function(){
+  const boton = document.querySelector('.dropdown-toggle');
+  const contenedor = document.querySelector('.PrincipalContainerDropdown');
+ 
+  boton.addEventListener('click', () => {
+    contenedor.classList.toggle('aumentado');
+  });
+}
 export const ListaClientes = () => (
-
+<div className="col-12">
+  <div className="row">
+    <div className="PrincipalContainerDropdown">
     <div ClassName="ContainerDropdownClientes">
 <div className="dropdowndos">
   <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-bs-toggle="dropdown" aria-expanded="false">
     Tus Clientes
   </button>
-  <ul className="dropdown-menu" aria-labelledby="dropdownMenu2">
-    <li><button className="dropdown-item" type="button">Aqui estaría el nombre y apellido</button></li>
+  
+  <ul id="DropDownPrincipal"className="dropdown-menu" aria-labelledby="dropdownMenu2">
+    <li><button id="ListaDropdown" className="dropdown-item" type="button">Aqui estaría el nombre y apellido</button></li>
     </ul>
 </div>
     </div>
-
+    </div>
+    </div>
+    </div>
 )
+
