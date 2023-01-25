14f3b151a9a9ef6d040a8046603a4c4d43e7061d
claudiareyesjofre <claudia.reyes.joffre@gmail.com>
2023-01-25 23:59:06 +0000

    login con bakend

--- src/js/views/login.js
+++ src/js/views/login.js
@@ -1,87 +1,119 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "../../styles/login.css";
+import Cookies from 'universal-cookie';
+import {Link, NavLink} from 'react-router-dom';
+import axios from 'axios';
 
-import pokebola from "../../img/POKEBOLA.png";
-import { Link } from "react-router-dom";
 
 
 
-export const Login = ({email,password}) => {
+class Login extends React.Component {
 
-  /* login profesor */
-const [login, setLogin] = useState([]);
+    state={
+        form:{
+            "email":"",
+            "password":""
+        },
+        error:false,
+        errorMesg:""
+    }
+    // esta funcion es el manejo para que no envie con los input vacios
+    manejoSubmit=e=>{
+        e.preventDefault();
+    }
+
+    //atrapamos lo que escribimos  en los input y los guardamos en el state que esta vacio mas arriba 
+    manejadorChange = async e=>{
+        await this.setState({
+            form:{
+                ...this.state.form,
+                [e.target.name]: e.target.value
+            }
+        })
+        
+    }
+
+    //enviamos los datos a la Apiurl que esta la ruta en el services y ademas los datos del formulario
+    manejadorBoton=()=>{
+        let url=  "https://3000-lukasoy-backendpokegym-pgpg4zwa6kn.ws-us84.gitpod.io/login";
+        axios.post(url,this.state.form)
+        .then(response =>{
+            if(response.data.status === 200)
+            {
+                console.log(response);
+            
+               
+            }else
+            {
+
+                console.log(response.data);
+                this.setState({
+                    error : true,
+                    errorMesg: response.data.message
+                    
+                })
+            }
+        }).catch(error =>{
+           
+            this.setState({
+                error : true,
+                errorMesg: "Error: Al conectar al servicio api"
+            });
+        })
+    }
   
-
-function Actualizar() {
-var myHeaders = new Headers();
-myHeaders.append("Content-Type", "application/json");
-
-var raw = JSON.stringify({});
-
-var requestOptions = {
-method: 'GET',
-headers: myHeaders,
-body: raw,
-redirect: 'follow'
-};
-
-fetch("https://3000-lukasoy-backendpokegym-h2fgbcggp3w.ws-us83.gitpod.io/profesor", requestOptions)
-.then(response => response.text())
-.then(result => console.log(result))
-.catch(error => console.log('error', error));
-  }
-
-   useEffect(() => {
-    fetch("https://3000-lukasoy-backendpokegym-h2fgbcggp3w.ws-us83.gitpod.io/profesor")
-      .then((data) => data.json())
-      .then((response) => setLogin(response));
-  }, []);
-useEffect(()=>{
-  Actualizar();
-}, [login]);
-
-/* final login profesor */
-
-
-  return(
-    <div className="container login-page">
+    render() {
+      return (
+        <React.Fragment>
+<div className="container login-page">
       <div className="row">
         <div className="col-1 login-style">
           <div className="login-card">
             <h1 className="titulologin">
               LOGIN
             </h1>
-            <form className="login-form">
-            {login.map((item) => (
-              <React.Fragment key={item.email}>
-               <input
-                  type="text"
-                  placeholder={`email: ${item.email}`}
-                  required
-                />
-                <input
-                type="password"
-                placeholder={`Contraseña: ${item.password}`}
-                required
-              />
-              </React.Fragment>
-            ))}
+            
+            <form onSubmit={this.manejoSubmit} >
+            
+            <input type="text"  className="fadeIn second" name="email" onChange={this.manejadorChange} placeholder="login"/>
+            <input type="password"  className="fadeIn third" name="password" onChange={this.manejadorChange} placeholder="password"/>   
+            <input type="submit" className="fadeIn fourth" onClick={this.manejadorBoton} value="Log In"/>
+              </form>
+              
               <Link to="/recover" className="link link-style">
                   ¿Olvidaste tu contraseña?
                 </Link>
-              <button>Ingresar</button>
+              
               <div className="links-div">
                 <Link to="/register" className="link link-style">
                   ¿No tienes cuenta? Registrate
                 </Link>
               </div>
-            </form>
+            
           </div>
           <h5>
-            <img className="poke-img" src={pokebola} style={{ width: 75 }} />
+           
           </h5>
         </div>
       </div>
     </div>
-  )
-  };
+
+            {this.state.error === true &&
+           
+                <div className="alert alert-danger" role="alert">
+                        {this.state.errorMesg}
+                </div>
+            }
+
+      </React.Fragment>
+      );
+    }
+  }
+
+  export { Login };
+
+
+
+
+
+
