3bc848905d5af22e463ceb83a6bf078a3420b8c0
pzagmel <pedro.zagalm@gmail.com>
2023-02-02 21:53:18 +0000

    fetch de estudiantes en perfil profe

--- src/js/views/perfilprofe.js
+++ src/js/views/perfilprofe.js
@@ -9,57 +9,54 @@ import { InfoGym } from "../component/CardsINFOGYM";
 import { Footer } from "../component/footer";
 import { Footergp } from "../component/footer2";
 import { FichaPersonal } from "../component/FijaPersonal";
-import { ListaClientes } from "../component/PerfilProfesionalListaClientes";
+import { ListaClientes } from "../component/ListaClientes";
 import { Fichaevaluacion } from "../component/fichaevaluacion";
 
-
 export const Perfilprofe = () => {
-  
   const { store, actions } = useContext(Context);
   const [list, setList] = useState([]);
+
   useEffect(() => {
     var myHeaders = new Headers();
+    myHeaders.append("Authorization", `Bearer ${localStorage.getItem("token")}`);
     myHeaders.append("Content-Type", "application/json");
 
+    var raw = JSON.stringify({
+      rol: false,
+    });
+
     var requestOptions = {
-      method: "GET",
+      method: "POST",
       headers: myHeaders,
+      body: raw,
       redirect: "follow",
     };
-
     fetch(
-      "https://3000-lukasoy-backendpokegym-veqmyydcg1p.ws-us84.gitpod.io/ficha",
+      "https://3000-lukasoy-backendpokegym-veqmyydcg1p.ws-us85.gitpod.io/user",
       requestOptions
     )
       .then((data) => data.json())
       .then((response) => {
-        console.l;
-        setList(response);
-      });
+        console.log("response", response);
+        setList(response.user);
+      })
+      .catch((error) => console.log("error", error));
   }, []);
 
   return (
     <div>
-       <div className="container" id="bienvenido">
-                <img src={Bienvenido} style={{width:1300}}/>
-        </div>
-      <h1> PerfilProfe</h1>
-      <FichaPersonal />
-      <ListaClientes />
-      <div className="row rowfichaej">
-        {list.map((elm, i) => {
-          return (
-            <Fichaevaluacion
-              key={"elm" + i}
-              peso={elm.peso}
-              estatura={elm.estatura}
-              porcentaje_grasa={elm.porcentaje_grasa}
-              porcentaje_musculo={elm.porcentaje_musculo}
-              nivel={elm.nivel}
-            />
-          );
-        })}
+      <div className="container" id="bienvenido">
+        <img src={Bienvenido} style={{ width: 1300 }} />
       </div>
+      <h1> PerfilProfe</h1>
+
+      <FichaPersonal />
+     
+     
+          <ListaClientes
+          data= {list}
+          />
+      
     </div>
   );
 };
